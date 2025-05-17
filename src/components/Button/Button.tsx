332b5aa53aab2d0837d6993ae493f1726b12562c
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-05-17 23:40:35 +0330

    test color

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,23 +1,9 @@
-// src/components/Button/Button.tsx
-import React from 'react';
+import styles from './_button.module.scss';
 
-type ButtonProps = {
+type Props = {
   label: string;
-  onClick?: () => void;
 };
 
-export const Button = ({ label, onClick }: ButtonProps) => (
-  <button
-    onClick={onClick}
-    style={{
-      padding: '8px 16px',
-      backgroundColor: '#1E90FF',
-      color: 'white',
-      border: 'none',
-      borderRadius: '4px',
-      cursor: 'pointer',
-    }}
-  >
-    {label}
-  </button>
-);
+export const Button = ({ label }: Props) => {
+  return <button className={styles.button}>{label}</button>;
+};
