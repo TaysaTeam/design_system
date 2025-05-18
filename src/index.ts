39e96892f6f1f77ac13149640490c6e9079b8746
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-05-18 08:53:06 +0330

    .

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './components/Button';
+import './styles/main.scss';
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,38 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), dts()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'MyDesignSystem',
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "MyDesignSystem",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
   },
+  css: {
+    preprocessorOptions: {
+    scss: {
+  additionalData: `@use "@styles/_colors.scss" as *;`
+}
+
+    },
+  },
+  resolve: {
+  alias: {
+    '@styles': path.resolve(__dirname, './src/styles'),
+  },
+},
+
 });
