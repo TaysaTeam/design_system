b0f7a667daf3dc5f42c96f531707a8cc1c3c3251
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-05-18 09:44:17 +0330

    .

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'tsup';
+import copy from 'esbuild-plugin-copy';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['esm'],
   dts: true,
   clean: true,
+  outDir: 'dist',
+  esbuildPlugins: [
+    copy({
+      assets: [
+        {
+          from: ['./src/styles/**/*'],
+          to: ['./dist/styles'],
+        },
+      ],
+      watch: true,
+    }),
+  ],
 });
