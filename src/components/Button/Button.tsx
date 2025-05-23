74c552b2ac8fd41dda25a951ade3909c7e349b9c
MrDevIo002 <mrdev.io@outlook.com>
2025-05-23 12:30:59 +0330

    Buttons design_system

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,26 @@
-import styles from './_button.module.scss';
+import styles from "./_button.module.scss";
 
 type Props = {
   label: string;
+  variant?: "primary" | "secondary";
+  type?: "contained" | "outlined" | "text";
+  size?: "large" | "medium" | "small";
+  disabled?: boolean;
 };
 
-export const Button = ({ label }: Props) => {
-  return <button className={styles.button}>{label}</button>;
+export const Button = ({
+  label,
+  variant = "primary",
+  type = "contained",
+  size = "medium",
+  disabled = false,
+}: Props) => {
+  return (
+    <button
+      className={`${styles.button} ${styles[variant]} ${styles[type]} ${styles[size]}`}
+      disabled={disabled}
+    >
+      {label}
+    </button>
+  );
 };
