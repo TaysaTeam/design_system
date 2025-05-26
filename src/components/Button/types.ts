62db7aef056769f88f202209424b957de32d617b
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-05-26 16:26:52 +0330

    create button types

---
+++ src/components/Button/types.ts
@@ -0,0 +1,16 @@
+import { PropsWithChildren } from "react";
+import { IconName } from "../Icon";
+
+type Variant = "primary" | "secondary";
+type type = "contained" | "outlined" | "text";
+type size = "large" | "medium" | "small";
+
+export type ButtonProps = PropsWithChildren & {
+  label: string;
+  variant: Variant;
+  type?: type;
+  size?: size;
+  disabled?: boolean;
+  leftIcon?: IconName;
+  rightIcon?: IconName;
+};
