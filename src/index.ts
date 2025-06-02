7b5ab1400fd89c68191ee9c169ce7b260312a42b
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-06-03 01:56:32 +0330

    add export component to index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export * from './components/Button';
+export * from "./components/Button";
+export * from "./components/Input";
+export * from "./components/Switch";
+export * from "./components/Textarea";
+export * from "./components/IconButton";
+export * from "./components/Icon";
