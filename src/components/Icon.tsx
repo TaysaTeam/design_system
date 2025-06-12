e8ca0a0cf908db92062b2e4b94db0e5958b33c76
MrDevIo002 <mrdev.io@outlook.com>
2025-06-12 17:06:05 +0330

    addIcons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -28,6 +28,14 @@ import { Instagram } from "./Icons/Instagram";
 import { Email } from "./Icons/Email";
 import { Location } from "./Icons/Location";
 import { ArrowLeft } from "./Icons/ArrowLeft";
+import { Calendar_Edit } from "./Icons/Calendar_Edit";
+import { Graph } from "./Icons/Graph";
+import { Notification_Bing } from "./Icons/Notification_Bing";
+import { Security_Safe } from "./Icons/Security_Safe";
+import { Smileys } from "./Icons/Smileys";
+import { Tick_Circle } from "./Icons/Tick_Circle";
+import { Timer } from "./Icons/Timer";
+import { User_Circle_Add } from "./Icons/User_Circle_Add";
 
 const icons = {
   add: Add,
@@ -59,6 +67,14 @@ const icons = {
   longCurvedLine: LongCurvedLine,
   shortCurvedLine: ShortCurvedLine,
   arrowLeft: ArrowLeft,
+  calendar_edit: Calendar_Edit,
+  graph: Graph,
+  notification_bing: Notification_Bing,
+  security_safe: Security_Safe,
+  smileys: Smileys,
+  tick_circle: Tick_Circle,
+  timer: Timer,
+  user_circle_add: User_Circle_Add,
 };
 
 export type IconName = keyof typeof icons;
